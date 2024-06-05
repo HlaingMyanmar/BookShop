bf892f4d90196159dbd53157f561c620f0af935d
Maung Naing <mgnaingterry@gamil.com>
2024-06-05 15:46:20 +0630

    Update

--- src/main/java/sspd/bookshop/models/Sale.java
+++ src/main/java/sspd/bookshop/models/Sale.java
@@ -5,25 +5,58 @@ import java.time.DateTimeException;
 
 public class Sale {
 
-    private int orderid;
+    private int saleid;
 
     private Timestamp date;
     private String cuname;
     private String cuphone;
     private int total;
 
-    public int getOrderid() {
-        return orderid;
+    public Sale(int saleid,String cuname,Timestamp date,String cuphone,int total){
+        this.saleid = saleid;
+        this.cuname =cuname;
+        this.date = date;
+        this.cuphone=cuphone;
+        this.total = total;
+    }
+
+    public int getSaleid() {
+        return saleid;
     }
 
     public void setOrderid(int orderid) {
-        this.orderid = orderid;
+        this.saleid = orderid;
     }
 
-    public int getCuname(){
+    public Timestamp getDate() {
+        return date;
+    }
+
+    public void setDate(Timestamp date) {
+        this.date = date;
+    }
+
+    public String getCuname() {
         return cuname;
     }
 
-    public void setCuname(String )
+    public void setCuname(String cuname) {
+        this.cuname = cuname;
+    }
 
+    public String getCuphone() {
+        return cuphone;
+    }
+
+    public void setCuphone(String cuphone) {
+        this.cuphone = cuphone;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
 }
