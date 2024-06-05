dbe6378916c2b92fb21e11224a90ff2a34e082a2
Maung Naing <mgnaingterry@gamil.com>
2024-06-05 15:37:29 +0630

    Update

--- src/main/java/sspd/bookshop/models/Book.java
+++ src/main/java/sspd/bookshop/models/Book.java
@@ -3,4 +3,52 @@ package sspd.bookshop.models;
 public class Book {
 
     private String bookname;
+    private String bookid;
+    private int quantity;
+    private int price;
+
+    public Book(String bookname,String bookid,int quantity,int price){
+
+        this.bookname = bookname;
+        this.bookid = bookid;
+        this.quantity = quantity;
+        this.price = price;
+
+
+    }
+
+    public String getBookname() {
+        return bookname;
+    }
+
+    public void setBookname(String bookname) {
+        this.bookname = bookname;
+    }
+
+    public String getBookid() {
+        return bookid;
+    }
+
+    public void setBookid(String bookid) {
+        this.bookid = bookid;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+
+
 }
---
+++ src/main/java/sspd/bookshop/models/Sale.java
@@ -0,0 +1,27 @@
+package sspd.bookshop.models;
+
+import java.security.Timestamp;
+import java.time.DateTimeException;
+
+public class Sale {
+
+    private int orderid;
+
+    private Timestamp date;
+    private String cuname;
+    private String cuphone;
+    private int total;
+
+    public int getOrderid() {
+        return orderid;
+    }
+
+    public void setOrderid(int orderid) {
+        this.orderid = orderid;
+    }
+
+    public int getCuname(){
+
+    }
+
+}
