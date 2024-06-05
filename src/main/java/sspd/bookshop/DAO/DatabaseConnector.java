49127ccf1464ec1becb01dbef78086582ba17fc8
SSPD Service And IT <hlainghtun2018@gmail.com>
2024-06-05 23:42:18 +0630

    update

--- src/main/java/sspd/bookshop/DAO/DatabaseConnector.java
+++ src/main/java/sspd/bookshop/DAO/DatabaseConnector.java
@@ -1,20 +1,55 @@
 package sspd.bookshop.DAO;
 
 
-import java.sql.Connection;
+import java.sql.*;
 
 public class DatabaseConnector {
 
-    private DatabaseConnector instance;
+    private static final String DB_DRIVER = "con.mysql.jdbc.Driver";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/studb";
+    private static final String DB_USER = "root";
+    private static final String DB_PASS = "";
 
-    private Connection con;
+    private static DatabaseConnector instance = null;
+
+    private Connection con =null;
 
     private DatabaseConnector(){
 
+        try {
+
+            Class.forName(DB_DRIVER);
+
+            con = DriverManager.getConnection(DB_URL, DB_USER, DB_DRIVER);
+
+            if(con!=null) {
+
+                System.out.println("Connetion successful");
+            }
+
+            else
+            {
+
+                System.out.println("Connetion Not successful");
+            }
+
+
+
+        } catch (ClassNotFoundException e) {
+
+            System.out.println("ClassNotFound Error");
+
+
+        } catch (SQLException e) {
+
+            System.out.println("SQLException Error");
+        }
+
+
+
+
 
     }
-
-
 
     public DatabaseConnector getDatabaseConnector(){
 
@@ -22,5 +57,72 @@ public class DatabaseConnector {
             instance = new DatabaseConnector();
         }
         return instance;
+    }
+
+    public static void getCloseConnection(Connection con,PreparedStatement stmt ){
+
+        if(con!=null) {
+
+            try {
+                con.close();
+            } catch (SQLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+        }
+
+        if(stmt!=null) {
+
+            try {
+                stmt.close();
+            } catch (SQLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+    public static void getCloseConnection(Connection con, PreparedStatement stmt , ResultSet set){
+
+        if(con!=null) {
+
+            try {
+                con.close();
+            } catch (SQLException e) {
+
+                e.printStackTrace();
+            }
+
+        }
+
+        if(stmt!=null) {
+
+            try {
+
+                stmt.close();
+
+            } catch (SQLException e) {
+
+                e.printStackTrace();
+            }
+        }
+
+        if(set!=null) {
+
+            try {
+
+                set.close();
+
+            } catch (SQLException e) {
+
+
+
+                e.printStackTrace();
+            }
+        }
+
+
+
+
     }
 }
