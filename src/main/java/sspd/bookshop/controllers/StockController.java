9b04abf85e65359e4dbb170ee6d209cdf30d2069
SSPD Service And IT <hlainghtun2018@gmail.com>
2024-07-25 20:41:33 +0630

    update

--- src/main/java/sspd/bookshop/controllers/StockController.java
+++ src/main/java/sspd/bookshop/controllers/StockController.java
@@ -132,7 +132,7 @@ public class StockController implements Initializable {
                 .mapToDouble(Book::getTotal)
                 .sum();
         NumberFormat currencyFormat = NumberFormat.getCurrencyInstance(new Locale("my", "MM"));
-        lbTotal.setText(currencyFormat.format(sum));
+        lbTotal.setText(convertToMyanmarCurrency(sum));
     }
 
     private void addTextFieldListener(TextField textField, FilteredList<Book> filteredData) {
@@ -174,6 +174,37 @@ public class StockController implements Initializable {
 
 
     }
+
+    private String convertToMyanmarCurrency(double amount) {
+        long kyat = (long) amount;
+        long lan = kyat / 100000;
+        kyat = kyat % 100000;
+        long thousandTen = kyat / 10000;
+        kyat = kyat % 10000;
+        long thousand = kyat / 1000;
+        kyat = kyat % 1000;
+        long hundred = kyat / 100;
+        kyat = kyat % 100;
+
+        StringBuilder result = new StringBuilder();
+
+        if (lan > 0) {
+            result.append(lan).append(" သိန်း ");
+        }
+        if (thousandTen > 0) {
+            result.append(thousandTen).append(" သောင်း ");
+        }
+        if (thousand > 0) {
+            result.append(thousand).append(" ထောင် ");
+        }
+        if (hundred > 0) {
+            result.append(hundred).append(" ရာ ");
+        }
+        result.append(kyat).append(" ကျပ်");
+
+        return result.toString();
+    }
+
 }
 
 
